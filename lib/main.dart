cc2e1d7434cbaf1b1df6ec283e04abfa152c0b2e
Nick Tse <nicholashtse@gmail.com>
2023-06-18 18:06:07 -0400

    add MovieModel class for easier data access

--- lib/main.dart
+++ lib/main.dart
@@ -50,9 +50,15 @@ class _MoviesListingState extends State<MoviesListing> {
     // print(apiResponse.body);
 
     if (apiResponse.statusCode == 200) {
+      // jsonDecode takes the response's body and parses it into the Dart collection Map
+      // it returns Map as Future, which are objects that return the results of the asynchronous operation
       final decodedResponse = jsonDecode(apiResponse.body);
       setState(() {
-        movies = decodedResponse['results'];
+        List<dynamic> results = decodedResponse['results'];
+        for (var element in results) {
+          movies?.add(MovieModel.fromJson(element));
+        }
+        // movies = decodedResponse['results'];
       });
     }
   }
@@ -72,5 +78,50 @@ class _MoviesListingState extends State<MoviesListing> {
             : const Center(child: CircularProgressIndicator()),
       ),
     );
+    // return Scaffold(
+    //   body: ListView.builder(
+    //     itemCount: movies?.length ?? 0,
+    //     itemBuilder: (context, index) {
+    //       return Padding(
+    //         padding: const EdgeInsets.all(8.0),
+    //         child: Text(movies?[index]["title"] ?? ''),
+    //       );
+    //     },
+    //   ),
+    // );
   }
+}
+
+//JSON response is converted into MovieModel object
+class MovieModel {
+  final int id;
+  final num popularity;
+  final int voteCount;
+  final bool video;
+  final String posterPath;
+  final String backdropPath;
+  final bool adult;
+  final String originalLanguage;
+  final String originalTitle;
+  final List<dynamic> genreIds;
+  final String title;
+  final num voteAverage;
+  final String overview;
+  final String releaseDate;
+
+  MovieModel.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        popularity = json['popularity'],
+        voteCount = json['voteCount'],
+        video = json['video'],
+        posterPath = json['posterPath'],
+        adult = json['adult'],
+        originalLanguage = json['originalLanguage'],
+        originalTitle = json['originalTitle'],
+        genreIds = json['genreIds'],
+        title = json['title'],
+        voteAverage = json['voteAverage'],
+        overview = json['overview'],
+        releaseDate = json['releaseDate'],
+        backdropPath = json['backdropPath'];
 }
